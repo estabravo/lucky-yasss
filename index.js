2572f584865b9adc9d3baf2f7037853ba0a4a812
estabravo <jbravo.post@gmail.com>
2021-05-27 20:01:55 -0700

    restructured

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ function getNum() {
 class NumPicker extends React.Component {
     render() {
         const num = getNum();
+        let msg;
+        if (num == 7) {
+            msg = 
+            <div>
+                <h2>YASSS!!</h2>
+                <img src="https://media1.tenor.com/images/f3321d0fc2b66c005cae93f9f0846b7c/tenor.gif?itemid=5498264" />
+            </div>
+        } else {
+            msg = <p>Sorry - Try again!</p>
+        }
         return (
             <div>
                 <h1>Your number is {num} </h1>
-                <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
+                {/* <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
                 {
                     num === 7 && 
                     <img src="https://media1.tenor.com/images/f3321d0fc2b66c005cae93f9f0846b7c/tenor.gif?itemid=5498264" />
-                }
+                } */}
+                {msg}
             </div>
                 
         )
